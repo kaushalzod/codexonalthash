3144ec0f621e3872a206f97d07a4f153ab2292ba
Mohit Patel <mohitpatel@clinikk.com>
2021-09-16 09:40:55 +0530

    added news and token

--- src/app/website/pages/about/about.component.ts
+++ src/app/website/pages/about/about.component.ts
@@ -12,52 +12,99 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
   token = [
     {
-      img:"assets/Images/about/img_1.png",
-      title: 'BUNK',
-      subtitle: "HTMLBunker is the first exchange in the world totally dedicated to HTMLCoin with more than 30 top pairs. HTMLBunker arose from the concept and need to contribute to the HTMLCoin community and crypto community as a whole."
+      img:"assets/Images/about/AutoSalesWear.webp",
+      title: 'AutoSalesWear (ASW)',
+      subtitle: "Auto Sales Wear is a startup company that is working with an (undisclosed) printing company to create the 'highest quality shirts users can find'. These limited editions shirts will be truly authentic with a numbered, autographed certificate.",
+      website: 'https://www.autosaleswear.com/',
+      whitepaper: 'https://www.autosaleswear.com/whitepaper'
     },
     {
-      img:"assets/Images/about/img_2.png",
-      title: 'XOLA',
-      subtitle: "A free-to-community project built using contract-oriented Solidity on the Althash platform. Designed to be a flexible, configurable, yet secure HRC20 Token Smart Contract with XOLA Protection Factor (Simulated Proof-of-Stake)."
-    },
-    {
-      img:"assets/Images/about/img_3.png",
-      title: 'Codex (CDEX)',
-      subtitle: "Codex token is used in the Codex ecosystem, primarily as an access token to VIP features on the Codex Wallet Bot."
-    },
-    {
-      img:"assets/Images/about/img_1.png",
-      title: 'BUNK',
-      subtitle: "HTMLBunker is the first exchange in the world totally dedicated to HTMLCoin with more than 30 top pairs. HTMLBunker arose from the concept and need to contribute to the HTMLCoin community and crypto community as a whole."
-    },
-    {
-      img:"assets/Images/about/img_2.png",
-      title: 'XOLA',
-      subtitle: "A free-to-community project built using contract-oriented Solidity on the Althash platform. Designed to be a flexible, configurable, yet secure HRC20 Token Smart Contract with XOLA Protection Factor (Simulated Proof-of-Stake)."
-    },
-    {
-      img:"assets/Images/about/img_3.png",
-      title: 'Codex (CDEX)',
-      subtitle: "Codex token is used in the Codex ecosystem, primarily as an access token to VIP features on the Codex Wallet Bot."
+      img:"assets/Images/about/Roy.webp",
+      title: 'Roy',
+      subtitle: "Roy starts with the token used in the shopping malls and will extend to the use of music lessons and psychological counseling. The reason for this is to reduce speculation and drive real utility.",
+      website: 'https://roycoin.co.kr/',
+      whitepaper: 'https://roycoin.co.kr/wp-content/uploads/2018/09/white-paperRoy-1.3En-3.pdf'
     },
     {
       img:"assets/Images/about/img_1.png",
       title: 'BUNK',
-      subtitle: "HTMLBunker is the first exchange in the world totally dedicated to HTMLCoin with more than 30 top pairs. HTMLBunker arose from the concept and need to contribute to the HTMLCoin community and crypto community as a whole."
+      subtitle: "HTMLBunker is the first exchange in the world totally dedicated to HTMLCoin with more than 30 top pairs. HTMLBunker arose from the concept and need to contribute to the HTMLCoin community and crypto community as a whole.",
+      website: 'https://htmlbunker.com/',
+      whitepaper: ''
     },
     {
       img:"assets/Images/about/img_2.png",
       title: 'XOLA',
-      subtitle: "A free-to-community project built using contract-oriented Solidity on the Althash platform. Designed to be a flexible, configurable, yet secure HRC20 Token Smart Contract with XOLA Protection Factor (Simulated Proof-of-Stake)."
+      subtitle: "A free-to-community project built using contract-oriented Solidity on the Althash platform. Designed to be a flexible, configurable, yet secure HRC20 Token Smart Contract with XOLA Protection Factor (Simulated Proof-of-Stake).",
+      website: 'https://xoladev.github.io/',
+      whitepaper: ''
     },
     {
-      img:"assets/Images/about/img_3.png",
+      img:"assets/Images/about/BIFF.webp",
+      title: 'BIFF/BIFD/BCDX',
+      subtitle: "The Biffy family of tokens was created for fun. Initially, the fun was simply in the name. Since then, it has grown into the learning of contract programming, rewards in tokens, and rewards in htmlcoin.",
+      website: 'http://biffytoken.site/token_factory.php',
+      whitepaper: 'http://biffytoken.site/white_paper.php'
+    },
+    {
+      img:"assets/Images/about/Photizo.webp",
+      title: 'Photizo (PHO)',
+      subtitle: "Photizo is an app focused on spreading bible knowledge amongst the wide crypto community. Users earn 'PHO' by answering questions correctly.",
+      website: 'https://althash.org/',
+      whitepaper: ''
+    },
+    {
+      img:"assets/Images/about/Cryptic_Mag.webp",
+      title: 'Cryptic Mag (MAG)',
+      subtitle: "Involved in the redeeming of rewards found inside your Cryptic Magazine. To redeem your prize view the 'redeem' tab, then enter the code found inside your magazine along with your destination wallet.",
+      website: 'https://althash.org/',
+      whitepaper: ''
+    },
+    {
+      img:"assets/Images/about/Codex.png",
       title: 'Codex (CDEX)',
-      subtitle: "Codex token is used in the Codex ecosystem, primarily as an access token to VIP features on the Codex Wallet Bot."
-    }
+      subtitle: "Codex token is used in the Codex ecosystem, primarily as an access token to VIP features on the Codex Wallet Bot.",
+      website: 'https://codextoken.io/',
+      whitepaper: 'https://e1087d92-cb3d-44ef-ae3a-57d3801a9a3c.filesusr.com/ugd/b6a441_93d42c270efb4e2fbe499c34c316f90f.pdf'
+    },
+    {
+      img:"assets/Images/about/Rain.webp",
+      title: 'Rain (RAIN)',
+      subtitle: "Rain token was created to be utilized in testing of the 'Codex Wallet Bot'. 'RAIN' will also be earned in the future by using the / rain function, which will mine 1 RAIN token based on size of rain.",
+      website: 'https://t.me/HRC20_Token_Room',
+      whitepaper: ''
+    },
+    {
+      img:"assets/Images/about/Love_Smile.webp",
+      title: 'Love & Smile (LOVE / SMILE)',
+      subtitle: "10 Smiles = 1 Love. And 1 Love = 1 HTMLCoin. Users get airdrops of Smile via codex bot, or you can also buy 5 smiles with 1 HTMLCoin. You can exchange Smile with Love and then You can Hold or exchange love with HTMLCoin. If you buy Smile that qty will be burned. If users donate again it will increase the value of Love.",
+      website: 'https://t.me/smilenlove',
+      whitepaper: ''
+    },
+    {
+      img:"assets/Images/about/Super_Fun.webp",
+      title: 'Super Fun (SFUN)',
+      subtitle: "REWARD People for Joke Telling: Knock Knock Jokes, One-Liner, Riddles,Puns, Wisdom/Knowledge Q&A, Teasers, WITTY videos, memes & the Likes.",
+      website: 'https://superfuntoken.com/',
+      whitepaper: 'https://superfuntoken.com/wp-content/uploads/2020/06/SUPERFUN-TOKEN-WHITEPAPER2020-1.docx'
+    },
+    {
+      img:"assets/Images/about/BURQ.webp",
+      title: 'Al-Buraq (BURQ)',
+      subtitle: "The main purpose of this token is to spread awareness of Holy Quran to Muslims. Also this token will be used for Al-Quran Online teachers. call centers and Individuals who are teaching online to students abroad and within country will be able to use this token to get their honorarium.",
+      website: 'https://al-buraq.net/',
+      whitepaper: 'https://al-buraq.net/whitepaper/'
+    },
+    {
+      img:"assets/Images/about/SuperAble.webp",
+      title: 'SuperAble (ABLE)',
+      subtitle: "Farmers are given x amount of Token as a consolation for their  big contribution to Food Supply in their village & community. This token can be a souvenir/collection or you can use it as a Utility Token or coupon for discounts.",
+      website: 'https://superable.net/',
+      whitepaper: ''
+    },
   ]
 
 }
--- src/app/website/pages/news/news.component.ts
+++ src/app/website/pages/news/news.component.ts
@@ -9,6 +9,99 @@ export class NewsComponent implements OnInit {
 
   constructor() { }
 
+  news = [
+    {
+      title: 'The CDEXplorer is completed. A rebuilt version of the HTML explorer',
+      dateAdded: 'June 2, 2020',
+      link: 'http://cdexplorer.net/',
+      image: 'assets/Images/news/CDEXplorer.webp'
+    },
+    {
+      title: 'Graviex Lists CDEX with pairs in ETH, DOGE, & BTC ',
+      dateAdded: 'April 2, 2020',
+      link: 'https://graviex.net/markets/cdexdoge',
+      image: 'assets/Images/news/Graviex_lists_CDEX.webp'
+    },
+    {
+      title: 'Completion of the 5th Monthly Airdrop for VIPs',
+      dateAdded: 'Mar. 31, 2020',
+      link: 'https://twitter.com/Codex_HTML/status/1245094639570427906?s=20',
+      image: 'assets/Images/news/Completion_of_the_5th_Monthly.webp'
+    },
+    {
+      title: 'Completion of the 4th Monthly Airdrop for VIPs',
+      dateAdded: 'Feb 27, 2020',
+      link: 'https://twitter.com/Codex_HTML/status/1233156744760741894?s=20',
+      image: 'assets/Images/news/Completion_of_the_4th_Monthly.webp'
+    },
+    {
+      title: 'Completion of the 3rd Monthly Airdrop for VIPs',
+      dateAdded: 'Jan. 30, 2020',
+      link: 'https://twitter.com/Codex_HTML/status/1222920729831858176?s=20',
+      image: 'assets/Images/news/Completion_of_the_3th_Monthly.webp'
+    },
+    {
+      title: "'CDEX' Token gets Listed on STEX Cryptocurrency Exchange​",
+      dateAdded: 'Jan. 25, 2020',
+      link: 'https://twitter.com/StexExchangeR/status/1221052433033584641?s=20',
+      image: 'assets/Images/news/STEX_Cryptocurrency_Exchange​.webp'
+    },
+    {
+      title: "Monthly CodexWalletBot Updates on the 1st Thursday of each month!​",
+      dateAdded: 'Jan. 1, 2020',
+      link: 'https://twitter.com/Codex_HTML/status/1217463556045668353?s=20',
+      image: 'assets/Images/news/Monthly_CodexWalletBot_Updates.webp'
+    },
+    {
+      title: "Milestones for HTMLCoin in 2019",
+      dateAdded: 'Jan. 1, 2020',
+      link: 'https://twitter.com/HTMLCOIN/status/1212337237398097920?s=20',
+      image: 'assets/Images/news/Milestones_for_HTMLCoin_in_2019.webp'
+    },
+    {
+      title: "Completion of Second Ever Monthly Airdrop for VIPs",
+      dateAdded: 'Dec. 28, 2019',
+      link: 'https://twitter.com/Codex_HTML/status/1210917109569601536?s=20',
+      image: 'assets/Images/news/Completion_of_Second_Ever_Monthly_Airdrop_for_VIPs.webp'
+    },
+    {
+      title: "Release of 'CDEX' Monthly Airdrops for VIPs on the CodexWalletBot",
+      dateAdded: 'Nov. 29, 2019',
+      link: 'https://twitter.com/Codex_HTML/status/1200550841423400960?s=20',
+      image: 'assets/Images/news/VIP_MONTHLY_AIRDROPS.webp'
+    },
+    {
+      title: "'CDEX' gets listed on CoinMarketCap",
+      dateAdded: 'Oct. 17, 2019',
+      link: 'https://coinmarketcap.com/currencies/codex/',
+      image: 'assets/Images/news/CDEX_Coinmarketcap.webp'
+    },
+    {
+      title: "Althash University is Added to Codex Official Website",
+      dateAdded: 'Oct. 1st, 2019',
+      link: 'https://www.codexonalthash.com/althash-university-initiative',
+      image: 'assets/Images/news/Althash_University_is_Added.webp'
+    },
+    {
+      title: "Codex Freelance Forum is released",
+      dateAdded: 'Sept. 15, 2019',
+      link: 'https://www.codexonalthash.com/forum',
+      image: 'assets/Images/news/Codex_Forum.webp'
+    },
+    {
+      title: "'CodexWalletBot' Is Adopted By The Althash Community And Hits 350 Users",
+      dateAdded: 'June 22, 2019',
+      link: 'https://medium.com/@tonyazimmerman1980/codextoken-cdex-is-a-token-hrc-20-created-on-htmlcoins-blockchain-called-althash-7a149d37ef69',
+      image: 'assets/Images/news/CodexWalletBot_Is_Adopted_By_The_Althash.webp'
+    },
+    {
+      title: "Whitepaper And Roadmap are Completed",
+      dateAdded: 'May 20, 2019',
+      link: 'https://www.codexonalthash.com/roadmap',
+      image: 'assets/Images/news/Whitepaper_And_Roadmap.webp'
+    },
+  ]
+
   ngOnInit(): void {
   }
 
